cc7a2e28415a8fd12416286a6a777670486e2193
Alex Nunes <alexgaribalde_tb@hotmail.com>
2024-07-20 07:42:19 -0300

    Configuração do Projeto

---
+++ src/main/java/br/com/futurodev/M02S06/M02S06Application.java
@@ -0,0 +1,13 @@
+package br.com.futurodev.M02S06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M02S06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M02S06Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/futurodev/M02S06/M02S06ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.futurodev.M02S06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M02S06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
